5a2279eb08e35437ba7caf79d7bf1c1d2918621d
david andres ramirez <da.ramirezv@uniandes.edu.co>
2018-09-17 01:44:57 -0500

    comentar mas cosas que no sirve

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 //esta clase es la que genera y da uso a los recursos de express
-var createError = require("http-errors");
+////var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -23,19 +23,19 @@ app.use(express.static(path.join(__dirname, "front/build")));
 app.use("/", recursosRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+//app.use(function (req, res, next) {
+//  next(createError(404));
+//});
 
 // error handler
-app.use(function (err, req, res, next) {
+//app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  //res.locals.message = err.message;
+  //res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  //res.status(err.status || 500);
   //res.render("error");
-});
+//});
 
 module.exports = app;
