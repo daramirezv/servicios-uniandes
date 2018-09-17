b560e4ac38d01fad0e63de7a3426fabf5f70175d
david andres ramirez <da.ramirezv@uniandes.edu.co>
2018-09-17 03:57:24 -0500

    arreglo1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 //esta clase es la que genera y da uso a los recursos de express
-////var createError = require("http-errors");
+var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -11,8 +11,8 @@ var recursosRouter = require("./routes/tutor");
 var app = express();
 
 // view engine setup
-//app.set("views", path.join(__dirname, "views"));
-//app.set("view engine", "jade");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -23,19 +23,19 @@ app.use(express.static(path.join(__dirname, "front/build")));
 app.use("/", recursosRouter);
 
 // catch 404 and forward to error handler
-//app.use(function (req, res, next) {
-//  next(createError(404));
-//});
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-//app.use(function (err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  //res.locals.message = err.message;
-  //res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  //res.status(err.status || 500);
-  //res.render("error");
-//});
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 module.exports = app;
